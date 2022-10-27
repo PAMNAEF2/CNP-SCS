84c497cb9d1ae79ca8f4f2f54491bb575d2edb79
PAMNAEF2 <84408512+PAMNAEF2@users.noreply.github.com>
2022-10-27 19:11:26 +0200

    a

--- commands/oposiciones.js
+++ commands/oposiciones.js
@@ -10,6 +10,7 @@ module.exports = {
         const embed = new EmbedBuilder()
         .setTitle("🔐 | Oposiciones")
         .setDescription("Oposiciones cerradas hasta nuevo aviso")
+        .setImage("https://cdn.discordapp.com/attachments/952587106931318794/1035239014501978212/Copia_de_NUNCA_CONECTADO.gif")
         interaction.reply({ embeds: [embed]})
 	},
 };
